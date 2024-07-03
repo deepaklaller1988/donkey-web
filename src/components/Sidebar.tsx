1910327042d353fecabcf401b30f33f41c25c1c5
dinesh <dlaller1988@gmail.com>
2024-07-03 10:51:47 +0530

    popup

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -59,6 +59,125 @@ export default function Sidebar() {
                         </div>
                     </section>
                 </li>
+                <li className="pl-[15px]">
+                    <section className="flex items-center relative listTop10 bg-white/5 hover:bg-white/10 transition rounded-md">
+                        <span className="relative min-w-[50px] w-[50px]">
+                            <img className="rounded-md w-full" src="/assets/images/album1.jpg" alt="album" />
+                        </span>
+                        <b className="transition -left-[15px] absolute rounded-full w-[30px] h-[30px] flex items-center justify-center">1</b>
+                        <div className="w-full relative pr-16 pl-4">
+                            <h4 className="text-white uppercase font-semibold transition">Wealth Health is mine</h4>
+                            <ul className="text-white/40 flex gap-2">
+                                <li className="text-sm">TV</li>.
+                                <li className="text-sm">EP1</li>.
+                                <li className="text-sm">SS1</li>
+                            </ul>
+                            <label className="absolute right-3 top-1/2 -mt-[10px] rounded-full px-2 text-sm transition">2024</label>
+                        </div>
+                    </section>
+                </li>
+                <li className="pl-[15px]">
+                    <section className="flex items-center relative listTop10 bg-white/5 hover:bg-white/10 transition rounded-md">
+                        <span className="relative min-w-[50px] w-[50px]">
+                            <img className="rounded-md w-full" src="/assets/images/album1.jpg" alt="album" />
+                        </span>
+                        <b className="transition -left-[15px] absolute rounded-full w-[30px] h-[30px] flex items-center justify-center">1</b>
+                        <div className="w-full relative pr-16 pl-4">
+                            <h4 className="text-white uppercase font-semibold transition">Wealth Health is mine</h4>
+                            <ul className="text-white/40 flex gap-2">
+                                <li className="text-sm">TV</li>.
+                                <li className="text-sm">EP1</li>.
+                                <li className="text-sm">SS1</li>
+                            </ul>
+                            <label className="absolute right-3 top-1/2 -mt-[10px] rounded-full px-2 text-sm transition">2024</label>
+                        </div>
+                    </section>
+                </li>
+                <li className="pl-[15px]">
+                    <section className="flex items-center relative listTop10 bg-white/5 hover:bg-white/10 transition rounded-md">
+                        <span className="relative min-w-[50px] w-[50px]">
+                            <img className="rounded-md w-full" src="/assets/images/album1.jpg" alt="album" />
+                        </span>
+                        <b className="transition -left-[15px] absolute rounded-full w-[30px] h-[30px] flex items-center justify-center">1</b>
+                        <div className="w-full relative pr-16 pl-4">
+                            <h4 className="text-white uppercase font-semibold transition">Wealth Health is mine</h4>
+                            <ul className="text-white/40 flex gap-2">
+                                <li className="text-sm">TV</li>.
+                                <li className="text-sm">EP1</li>.
+                                <li className="text-sm">SS1</li>
+                            </ul>
+                            <label className="absolute right-3 top-1/2 -mt-[10px] rounded-full px-2 text-sm transition">2024</label>
+                        </div>
+                    </section>
+                </li>
+                <li className="pl-[15px]">
+                    <section className="flex items-center relative listTop10 bg-white/5 hover:bg-white/10 transition rounded-md">
+                        <span className="relative min-w-[50px] w-[50px]">
+                            <img className="rounded-md w-full" src="/assets/images/album1.jpg" alt="album" />
+                        </span>
+                        <b className="transition -left-[15px] absolute rounded-full w-[30px] h-[30px] flex items-center justify-center">1</b>
+                        <div className="w-full relative pr-16 pl-4">
+                            <h4 className="text-white uppercase font-semibold transition">Wealth Health is mine</h4>
+                            <ul className="text-white/40 flex gap-2">
+                                <li className="text-sm">TV</li>.
+                                <li className="text-sm">EP1</li>.
+                                <li className="text-sm">SS1</li>
+                            </ul>
+                            <label className="absolute right-3 top-1/2 -mt-[10px] rounded-full px-2 text-sm transition">2024</label>
+                        </div>
+                    </section>
+                </li>
+                <li className="pl-[15px]">
+                    <section className="flex items-center relative listTop10 bg-white/5 hover:bg-white/10 transition rounded-md">
+                        <span className="relative min-w-[50px] w-[50px]">
+                            <img className="rounded-md w-full" src="/assets/images/album1.jpg" alt="album" />
+                        </span>
+                        <b className="transition -left-[15px] absolute rounded-full w-[30px] h-[30px] flex items-center justify-center">1</b>
+                        <div className="w-full relative pr-16 pl-4">
+                            <h4 className="text-white uppercase font-semibold transition">Wealth Health is mine</h4>
+                            <ul className="text-white/40 flex gap-2">
+                                <li className="text-sm">TV</li>.
+                                <li className="text-sm">EP1</li>.
+                                <li className="text-sm">SS1</li>
+                            </ul>
+                            <label className="absolute right-3 top-1/2 -mt-[10px] rounded-full px-2 text-sm transition">2024</label>
+                        </div>
+                    </section>
+                </li>
+                <li className="pl-[15px]">
+                    <section className="flex items-center relative listTop10 bg-white/5 hover:bg-white/10 transition rounded-md">
+                        <span className="relative min-w-[50px] w-[50px]">
+                            <img className="rounded-md w-full" src="/assets/images/album1.jpg" alt="album" />
+                        </span>
+                        <b className="transition -left-[15px] absolute rounded-full w-[30px] h-[30px] flex items-center justify-center">1</b>
+                        <div className="w-full relative pr-16 pl-4">
+                            <h4 className="text-white uppercase font-semibold transition">Wealth Health is mine</h4>
+                            <ul className="text-white/40 flex gap-2">
+                                <li className="text-sm">TV</li>.
+                                <li className="text-sm">EP1</li>.
+                                <li className="text-sm">SS1</li>
+                            </ul>
+                            <label className="absolute right-3 top-1/2 -mt-[10px] rounded-full px-2 text-sm transition">2024</label>
+                        </div>
+                    </section>
+                </li>
+                <li className="pl-[15px]">
+                    <section className="flex items-center relative listTop10 bg-white/5 hover:bg-white/10 transition rounded-md">
+                        <span className="relative min-w-[50px] w-[50px]">
+                            <img className="rounded-md w-full" src="/assets/images/album1.jpg" alt="album" />
+                        </span>
+                        <b className="transition -left-[15px] absolute rounded-full w-[30px] h-[30px] flex items-center justify-center">1</b>
+                        <div className="w-full relative pr-16 pl-4">
+                            <h4 className="text-white uppercase font-semibold transition">Wealth Health is mine</h4>
+                            <ul className="text-white/40 flex gap-2">
+                                <li className="text-sm">TV</li>.
+                                <li className="text-sm">EP1</li>.
+                                <li className="text-sm">SS1</li>
+                            </ul>
+                            <label className="absolute right-3 top-1/2 -mt-[10px] rounded-full px-2 text-sm transition">2024</label>
+                        </div>
+                    </section>
+                </li>
             </ul>
         </div>
     );
