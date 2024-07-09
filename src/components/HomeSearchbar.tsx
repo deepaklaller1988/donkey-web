468127c7c37fad7e12a1464c6323b834de24e28a
dinesh <dlaller1988@gmail.com>
2024-07-09 18:07:57 +0530

    changes

--- src/components/HomeSearchbar.tsx
+++ src/components/HomeSearchbar.tsx
@@ -4,16 +4,18 @@ export default function HomeSearchbar() {
     return (
         <div className="searchBar w-full px-4">
         <form>
-        <section className="flex justify-between bg-black/70 rounded-full max-w-[550px] w-full m-auto">
+        <section className="flex justify-between bg-black/30 rounded-full max-w-[520px] m-auto">
             <span className="p-2">
-                <a href="/filters" className="rounded-full p-1 px-2 text-sm bg-white/30 hover:bg-white/40 transition text-white/60 flex items-center">
+                <a href="/filters" className="rounded-full p-1 px-2 text-sm bg-black/30 transition text-white/60 hover:text-white flex items-center">
                     <CiFilter className="mr-1" /> Filter
                 </a>
             </span>
             <input className="text-white w-full bg-white/0 text-center pr-10" placeholder="Search..." type="text" />
+            
             <button className="pColor mr-1 p-3">
                 <IoSearch className="text-xl" />
             </button>
+            
         </section>
             </form>
     </div>
