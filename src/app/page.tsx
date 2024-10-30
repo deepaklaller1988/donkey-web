22c5fe1cb240d8ee2e20520a50770be10323adef
Dinesh <dlaller1988@gmail.com>
2024-10-30 10:14:31 +0530

    Updated Code 30-10-2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,7 @@ export default function page() {
   // useEffect(() => {
   //   router.push("/dashboard");
   // }, []);
-  useEffect(() => {
-    // Check if there's anything in local storage
-    if (localStorage.length > 0) {
-      // Set a timer to clear local storage after 2 minutes (120,000 ms)
-      const timer = setTimeout(() => {
-        localStorage.clear();
-        console.log("Local storage cleared after 2 minutes");
-      }, 60000);
 
-      // Clean up the timer on component unmount
-      return () => clearTimeout(timer);
-    }
-  }, []);
   return (
     <div>
       <Loader />
