9d751aa7c3b312d68232c2f2fcd9bddc906dbfd8
dlaller <dlaller1988@gmail.com>
2026-02-09 11:03:24 +0530

    Remove all previous ad script with new one

--- src/components/core/AdSrcipt.tsx
+++ src/components/core/AdSrcipt.tsx
@@ -15,15 +15,16 @@ const AdScript = () => {
       scripts.push(script);
     };
 
-    // ✅ Galaksion Popunder
-    // loadScript("//sa.shanksmagh.com/rvUKZVqmljmA/NgvMR");
-    loadScript("//ob.algatyramin.com/rhDqkYurFPBdK2ten/134504");
+    // // ✅ Galaksion Popunder
+    // // loadScript("//sa.shanksmagh.com/rvUKZVqmljmA/NgvMR");
+    // loadScript("//ob.algatyramin.com/rhDqkYurFPBdK2ten/134504");
 
-    // ✅ Galaksion Native Shufflebox (Vignette)
-    loadScript("//iz.solionenzymes.com/s5cjs9YKq8l/134505");
+    // // ✅ Galaksion Native Shufflebox (Vignette)
+    // loadScript("//iz.solionenzymes.com/s5cjs9YKq8l/134505");
 
-    // ✅ Galaksion Video Ad
-    loadScript("//bx.simulasending.com/vtZj1yeasXrJq/134507");
+    // // ✅ Galaksion Video Ad
+    // loadScript("//bx.simulasending.com/vtZj1yeasXrJq/134507");
+    loadScript("//d33f51dyacx7bd.cloudfront.net/?aydfd=1241784");
 
     return () => {
       // Cleanup on unmount
